3c7af8a2c2a8fe938c60340aa48243a41de6399c
Yosuke Kono <n11230380@qut.edu.au>
2024-12-05 15:24:32 +1000

    [CHORE] small adjustments

--- resources/views/talks/index.blade.php
+++ resources/views/talks/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __("Talk List") }}
+            {{ __("Talks") }}
         </h2>
     </x-slot>
 
