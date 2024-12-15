80e96b8aa3fde407e1c7386572d4f4cb81c9aebf
Yosuke Kono <n11230380@qut.edu.au>
2024-12-15 23:13:16 +1000

    [FEAT] integrate with Github using Laravel Socialite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,8 @@
                 </a>
             @endif
 
+            <a href="/auth/redirect">Log in with Github</a>
+
             <x-primary-button class="ms-3">
                 {{ __("Log in") }}
             </x-primary-button>
